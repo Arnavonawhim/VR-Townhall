5a061006965a74aa7c9c00d821df9ab3f2f8bc19
Arnav Agrawal <agrawal.arnav20@gmail.com>
2026-03-13 14:09:31 +0530

    fixed unity not loading

--- meetverse-client/vite.config.js
+++ meetverse-client/vite.config.js
@@ -7,6 +7,15 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cache-Control": "no-cache",
     }
+  },
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url.endsWith(".data") || req.url.endsWith(".wasm") || req.url.endsWith(".symbols.json")) {
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      }
+      next();
+    });
   }
 })
