ec333e2e5b9577f73e9376fbff2c8c8c8cd2efad
Arnav Agrawal <agrawal.arnav20@gmail.com>
2026-03-13 16:22:51 +0530

    force vite.config out of lfs

--- meetverse-client/vite.config.js
+++ meetverse-client/vite.config.js
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b987d7d14d48cc17888758dbae3278e22f27420247f1072858a8f86a1b48eca
-size 274
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    }
+  }
+})
